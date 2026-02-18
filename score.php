84dfbae5a54cb8dc565e8b2ebdbddec5a56e53ec
BharathNags <bharathnags2002@gmail.com>
2026-02-18 06:45:16 +0000

    Ajax

---
+++ score.php
@@ -0,0 +1,8 @@
+<h1>India Vs Pakistan</h1>
+<h2>Batting</h2>
+<h4>Ishan Kishan-77</h4>
+<h4>Tilak Varma -32</h4>
+<h4>Surya kumar - 45</h4>
+<h4>Hardik Pandya - 0</h4>
+<h1>Batting</h1>
+<h2>balling</h2>
