99d33a1e11f7429b0a27bcb1128bb5efc456dcbd
Bharath nags <100501611+Bharathnags@users.noreply.github.com>
2026-04-01 12:32:04 +0530

    New Changes in Code

--- Training/CustomerTracker/Plugin/CustomerNamePlugin.php
+++ Training/CustomerTracker/Plugin/CustomerNamePlugin.php
@@ -1,35 +1,45 @@
 <?php
 namespace Training\CustomerTracker\Plugin;
 
-use Magento\Customer\Model\Customer;
+use Magento\Customer\CustomerData\Customer;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Psr\Log\LoggerInterface;
 
 class CustomerNamePlugin
 {
-    private $scopeConfig;
-    
-    public function __construct(ScopeConfigInterface $scopeConfig)
-    {
+    private ScopeConfigInterface $scopeConfig;
+    private LoggerInterface $logger;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        LoggerInterface $logger
+    ) {
         $this->scopeConfig = $scopeConfig;
+        $this->logger = $logger;
     }
-    
-    public function afterGetName(Customer $customer, $result)
+
+    public function afterGetSectionData(Customer $subject, $result)
     {
-      
-        $enabled = $this->scopeConfig->getValue('customertracker/general/enabled');
-        
+        $enabled = $this->scopeConfig->getValue(
+            'customertracker/general/enabled',
+            ScopeInterface::SCOPE_STORE
+        );
+
         if (!$enabled) {
             return $result;
         }
-        
-       
-        $prefix = $this->scopeConfig->getValue('customertracker/general/customer_prefix');
-        
-        
-        if ($prefix) {
-            return $prefix . ' ' . $result;
+
+        $prefix = $this->scopeConfig->getValue(
+            'customertracker/general/customer_prefix',
+            ScopeInterface::SCOPE_STORE
+        );
+
+        if ($prefix && isset($result['fullname'])) {
+            $result['fullname'] = $prefix . ' ' . $result['fullname'];
+            $this->logger->info('Customer name modified to: ' . $result['fullname']);
         }
-        
+
         return $result;
     }
 }
