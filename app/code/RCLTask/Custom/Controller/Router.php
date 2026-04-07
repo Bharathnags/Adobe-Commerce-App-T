14c8e0a889bbf82784e0100da0a19d89e5c97166
root <root@localhost>
2026-04-07 08:52:51 +0000

    Custom Router added

---
+++ app/code/RCLTask/Custom/Controller/Router.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RCLTask\Custom\Controller;
+
+use Magento\Framework\App\ActionFactory;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\App\RouterInterface;
+
+class Router implements RouterInterface
+{
+    protected $actionFactory;
+
+    public function __construct(ActionFactory $actionFactory)
+    {
+        $this->actionFactory = $actionFactory;
+    }
+
+    public function match(RequestInterface $request)
+    {
+      
+        if ($request->getModuleName() === 'custom') {
+            return null;
+        }
+        $identifier = trim($request->getPathInfo(), '/');
+
+        
+        if ($identifier === 'intern') {
+
+            $request->setModuleName('custom');
+            $request->setControllerName('index');
+            $request->setActionName('index');
+
+           
+            $request->setParam('name', 'Intern Name');
+
+            return $this->actionFactory->create(
+                \Magento\Framework\App\Action\Forward::class
+            );
+        }
+
+        return null;
+    }
+}
